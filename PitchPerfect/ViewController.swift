307cfaeae2874dde242718e0955945e4ea3957b6
Rufin Hounkpe <rhounkpe@gmail.com>
2020-06-05 21:18:44 +0200

    Adding code to 'Record' Button testing it

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -14,7 +14,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed...")
+    }
+    
 }
 
